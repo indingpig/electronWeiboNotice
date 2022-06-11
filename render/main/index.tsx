bcef5050e7e695936b1ed351ce815a141cfbd85f
ex-lihonghan <indoing@gmail.com>
2022-06-12 00:27:51 +0800

    优化代码

--- render/main/index.tsx
+++ render/main/index.tsx
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import { ipcRenderer } from 'electron';
-import { renderFetch } from './../common/renderHttps';
+import {renderFetch as myFecth } from './../common/renderHttps';
+import { Weibo, CardType } from './interface';
 
 const App = () => {
 	const [localCode, setLocalCode] = useState('');//本身的控制码
 	const login = async () => {
-		// 获取登陆后的控制码
-		// 因为登录状态是在主进程维护，通过主进程来处理ipc事件
-		// const code = await ipcRenderer.invoke('start', {aaa: 123});
 		let url = 'https://m.weibo.cn/api/container/getIndex';
 		const params = {
 			type: 'uid',
 			value: '5976999361',
 			containerid: '1076035976999361'
 		};
-		const code = await renderFetch.get(url, params);
-		console.log(code);
-
-		// 存储控制码
-		setLocalCode(code);
+		const {data}:Weibo = await myFecth.get(url, params);
+		let cardsFirst: CardType;
+		if (data.cards && data.cards.length > 1) {
+			cardsFirst = data.cards[1];
+			console.log(cardsFirst);
+		}
+		// setLocalCode(data);
 	};
 	return (
 		<div>
